1c947a2a34f31d8a3967d5d53dd0d1abafee65c5
jcalvin4 <joe.m.calvin@outlook.com>
2026-01-31 22:17:41 -0800

    Search By Genre

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1809f4c58362c29271ac2edfe3e0696b461ba437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f02edba08937480d965927e4ecc75d96350315")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
